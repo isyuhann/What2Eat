a69c8e07aa2c47c9600e18e0dd6e6b09f3086345
YuHan Huang <5monica.cupcake@gmail.com>
2025-11-26 23:28:54 +0800

    update

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-<<<<<<< Updated upstream
-=======
-
-declare module "*.png" {
-  const value: string;
-  export default value;
-}
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.svg";
->>>>>>> Stashed changes
